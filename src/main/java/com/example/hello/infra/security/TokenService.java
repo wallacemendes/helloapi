3aa913877b1f92162e6bb8ba1f71db22391b20db
wallacemendes <wallace.bmj@gmail.com>
2024-09-28 12:53:44 -0300

    adjustments

--- src/main/java/com/example/hello/infra/security/TokenService.java
+++ src/main/java/com/example/hello/infra/security/TokenService.java
@@ -16,7 +16,7 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    @Value("${api.security.token.secret}")
+    @Value("${app.jwtSecret}")
     private String secret;
 
     public String generateToken(User user){
